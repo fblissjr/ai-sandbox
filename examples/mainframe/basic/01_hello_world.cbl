efbcff95d340bd0d8c6fbb9b128917f11f582818
Fred Bliss <fbliss@gmail.com>
2024-02-10 09:11:46 -0600

    examples/mainframe/basic/01_hello_world.cbl - commit created by https://agenthub.dev/pipeline?run_id=CTd5yDGPBV3aRcrePUWoHH

---
+++ examples/mainframe/basic/01_hello_world.cbl
@@ -0,0 +1,18 @@
+```cobol
+       IDENTIFICATION DIVISION.                                     
+       PROGRAM-ID. HELLO.    
+       * This program is a simple demonstration of COBOL's capabilities.                                         
+      **
+       ENVIRONMENT DIVISION.
+      * This division specifies the configuration of the system environment.
+      ** 
+       DATA DIVISION.
+      * Here is where the program's variables would be declared. Currently, this program does not use any variables.
+      **
+       PROCEDURE DIVISION.  
+       * The following is the core logic of the program.                                         
+           DISPLAY 'HELLO WORLD'. 
+           * This line outputs the text "HELLO WORLD" to the user.                                   
+           STOP RUN.  
+           * This statement terminates the program.                                             
+```
